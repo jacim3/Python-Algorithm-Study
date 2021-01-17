b09581de5010ed627d1972ac2655959579569377
jack <kjk899@naver.com>
2021-01-17 22:44:51 +0900

    asdf

--- 섹션 6/1. 재귀함수란(이진수출력)/AA.py
+++ 섹션 6/1. 재귀함수란(이진수출력)/AA.py
@@ -1,6 +1,9 @@
 import sys
 # sys.stdin = open('input.txt','rt')
 
+
+# 1. Java에서 사용하는 방식을 이용
+"""
 n = int(input())
 list = []
 
@@ -18,3 +21,23 @@ trans(n)
 
 for i in reversed(list):
     print(i, end="")
+
+"""
+
+# 2. Python의 방식을 이용
+# 재귀함수의 원리는 Stack을 이용하여 함수의 호출 정보들을 기록을 한 이후, 순차적으로 함수가 작동하며, 이는 DFS(깊이우선)의 방식을 따른다.
+# 함수는 자신의 역할이 끝날 때 까지는 도중에 다른 함수가 호출된다 하더라도, 종료되지 않고 스택에 저장된다. 
+def DFS(x):
+    if x ==0:
+        return
+    else:
+        
+        DFS(x//2)
+        print(x%2, end=' ')
+
+# 인터프리터 내에서 직접 실행했을 때만, if문 내의 코드를 실행해라
+if __name__ == "__main__":
+    n = int(input())
+    DFS(n)
+
+
