472e6bdda19d56c38c73283b370ac6554d56a737
jacim3 <60813834+jacim3@users.noreply.github.com>
2021-03-22 18:44:11 +0900

    DFS Exercise

--- 섹션 7/4. 동전바꿔주기/AA.py
+++ 섹션 7/4. 동전바꿔주기/AA.py
@@ -1,31 +1,27 @@
 import sys
 # sys.stdin = open('input.txt','r')
-
-def DFS(level, sum, st):
-    if level == L:
-        if sum == 20:
-            total.append(st)
-    else:
-        DFS(level+1, sum+dic[level], st+str(dic[level])+" ")
-        DFS(level+1, sum, st)
+# 처음 생각했던 규칙이 맞았음. 2차원 배열에 담기
+def DFS(level, sum):
+    global count
+    if sum < 0:
+        return
+    if level == K:
+        if sum == 0:
+            count = count+1
+    else :
+        for i in range(len(dic[level])+1):
+            DFS(level+1, sum - i*dic[level][0])
 
 if __name__ == "__main__":
+    count = 0
     T = int(input())
     K = int(input())
     dic = []
-    L = 0
-    total = []
     for i in range(K):
+        arr = []
         P, N = map(int, input().split())
-   
-        L = L+N
-
-        for j in range(N):
-            dic.append(P)
-
-    
-    DFS(0,0,"")
-    # print(total)
-    sss = set(total)
-    # print(sss)
-    print(len(sss))
+        for i in range(N):
+            arr.append(P)
+        dic.append(arr)
+    DFS(0, T)
+    print(count)
